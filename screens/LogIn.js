c3c1445d17825157ce26ae87d0137e6fe6b479e8
hahuyhoang <huyhoang24062000@gmail.com>
2022-10-28 00:05:01 +0700

    newcode

--- screens/LogIn.js
+++ screens/LogIn.js
@@ -1,29 +1,89 @@
-import { View, Text, SafeAreaView, ImageBackground, Image, TextInput } from 'react-native'
-import React from 'react'
-
-const LogIn = () => {
+import { View, Text, SafeAreaView, ImageBackground, Image, TextInput, StyleSheet, TouchableOpacity, KeyboardAvoidingView } from 'react-native'
+import React, { useState } from 'react'
+import { Entypo } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+const LogIn = ({ navigation }) => {
+  const [getPasswordVisible, setPasswordVisible] = useState(false)
   return (
-    <SafeAreaView className="flex-1">
+    <SafeAreaView className="flex-1 bg-transparent">
+
       <View className="flex-1">
-        <ImageBackground className="w-screen h-screen" source={require('../assets/images/MaskGroup.png')} resizeMode="cover" />
-      </View>
-      <View className="flex-1 items-center">
-        <Image className="" source={require('../assets/images/Group.png')} />
-      </View>
-      <View className="flex-1 ml-5 mr-5">
-        <View>
-          <Text>Loging</Text>
-          <Text>Enter your emails and password</Text>
+        <View className="flex-1 " >
+          <ImageBackground className="w-screen h-screen" source={require('../assets/images/MaskGroup.png')} resizeMode="cover" />
+        </View>
+        <View className="flex-1 items-center">
+          <Image className="" source={require('../assets/images/Group.png')} />
         </View>
       </View>
-        <View className="flex-1 ml-5 mr-5">
+      <View className="flex-1">
+        <View className="ml-5 mr-5">
           <View>
-            <Text>Email</Text>
-            <TextInput className="border-b-2" placeholder='nhap email cua ban' />
+            <Text style={styles.font} className="text-2xl mb-3">Loging</Text>
+            <Text style={styles.font} className="text-gray-500">Enter your emails and password</Text>
           </View>
         </View>
+        <View className=" ml-5 mt-10  mr-5">
+          <View>
+            <Text style={styles.font} className="text-gray-500 mb-3">Email</Text>
+            <TextInput className="border-b border-b-gray-300" style={styles.font} placeholder='Nhap email cua ban' />
+          </View>
+          <View className="mt-8">
+            <Text style={styles.font} className="text-gray-500 mb-3">Password</Text>
+            <TextInput className="border-b border-b-gray-300 " style={styles.font} placeholder='Nhap password cua ban' secureTextEntry={getPasswordVisible ? false : true} />
+            <TouchableOpacity className="absolute right-0 bottom-1"
+              onPress={() => {
+                setPasswordVisible(!getPasswordVisible)
+              }}>
+              {
+                getPasswordVisible ?
+                  <Feather name="eye" size={24} color="gray" />
+                  :
+                  <Feather name="eye-off" size={24} color="gray" />
+              }
+            </TouchableOpacity>
+          </View>
+        </View>
+
+      </View>
+      <View className="ml-5 flex-1">
+        <View className="flex-1">
+          <View className=" ml-5 mr-5 items-center mt-3">
+            <TouchableOpacity className="ml-auto">
+              <Text style={styles.font} className="float-right text-gray-700">Forgot Password?</Text>
+            </TouchableOpacity>
+          </View>
+          <View className="mt-5 w-11/12  ">
+            <TouchableOpacity className="bg-green-500 items-center h-14 justify-center rounded-2xl" onPress={() => { navigation.navigate('Home') }}>
+              <View>
+                <Text style={styles.font} className="text-white text-lg">Get Started</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View className=" flex-1 items-center">
+          <View className="flex-row">
+            <Text style={styles.font}>
+              Don’t have an account?
+            </Text>
+            <TouchableOpacity>
+              <Text style={styles.font} className="ml-1 text-green-500">
+                Signup
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+
     </SafeAreaView>
+
+
+
   )
 }
 
 export default LogIn
+const styles = StyleSheet.create({
+  font: {
+    fontFamily: 'Gilroy-Light'
+  },
+})
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -2,49 +2,49 @@ import { View, Text, ImageBackground, Image, SafeAreaView, StyleSheet, Touchable
 import React from 'react'
 import { useFonts } from 'expo-font';
 
-const Welcome = ({navigation}) => {
-    let [fontsLoaded] = useFonts({
-        'Gilroy-Light': require('../assets/font/Gilroy-SemiBold.ttf')
-      });
-      if (!fontsLoaded) {
-        return console.log('loi roi kia')
-      }
-    return (
-        <SafeAreaView className="flex-1">
-            <View className="flex-1 w-screen h-screen">
-                <Image className="w-screen h-screen" source={require('../assets/images/back.jpg')} />
+const Welcome = ({ navigation }) => {
+  let [fontsLoaded] = useFonts({
+    'Gilroy-Light': require('../assets/font/Gilroy-SemiBold.ttf')
+  });
+  if (!fontsLoaded) {
+    return console.log('loi roi kia')
+  }
+  return (
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 w-screen h-screen">
+        <Image className="w-screen h-screen" source={require('../assets/images/back.jpg')} />
+      </View>
+      <View className="flex-1 pt-16 items-center">
+        <Image source={require('../assets/images/logo.png')} />
+        <Text style={styles.font} className="text-white text-2xl pt-1" >Welcome</Text>
+        <Text style={styles.font} className="text-white text-2xl">to our store</Text>
+        <Text style={styles.font} className="text-slate-300">Ger your groceries in as fast as one hour</Text>
+        <View className="mt-14 w-11/12">
+          <TouchableOpacity className="bg-green-500 items-center h-14 justify-center rounded-2xl" onPress={() => { navigation.navigate('Login') }}>
+            <View>
+              <Text style={styles.font} className="text-white text-lg">Get Started</Text>
             </View>
-            <View className="flex-1 pt-16 items-center">
-                <Image source={require('../assets/images/logo.png')} />
-                <Text style={styles.font} className="text-white text-2xl pt-1" >Welcome</Text>
-                <Text style={styles.font} className="text-white text-2xl">to our store</Text>
-                <Text style={styles.font} className="text-slate-300">Ger your groceries in as fast as one hour</Text>
-            <View className="mt-14 w-3/4">
-                <TouchableOpacity  className="bg-green-400 items-center h-16 justify-center rounded-2xl" onPress={() =>{navigation.navigate('Login')}}>
-                    <View>
-                        <Text style={styles.font} className="text-white text-lg">Get Started</Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
-            </View>
-        </SafeAreaView>
-    )
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 export default Welcome
 const styles = StyleSheet.create({
-    font: {
-      fontFamily: 'Gilroy-Light'
-    },
-    width: {
-      marginHorizontal: 20
-    },
-    icon: {
-      position: 'absolute',
-      top: '30%',
-      left: '2%'
-    },
-    button:{
-        width: '50%'
-    }
-  })
+  font: {
+    fontFamily: 'Gilroy-Light'
+  },
+  width: {
+    marginHorizontal: 20
+  },
+  icon: {
+    position: 'absolute',
+    top: '30%',
+    left: '2%'
+  },
+  button: {
+    width: '50%'
+  }
+})
